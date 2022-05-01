ef6b2cfce403e6a0232375030290728e8732c167
lizziluebke <77250113+lizziluebke@users.noreply.github.com>
2022-05-01 14:20:51 -0500

    profile component from textbook
    edited to use latest authenticator componenet

---
+++ src/profile.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+
+import { Authenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+
+function Profile() {
+  return (
+    <div style={containerStyle}>
+        <Authenticator>
+        {({ signOut, user }) => (
+        <main>
+            <h1>
+                Hello {user.username}
+            </h1>
+            {console.log(user)}
+            <h2> 
+                Email: {user.attributes.email}
+            </h2>
+
+            <Button 
+                onClick={signOut}
+            > 
+                Sign out
+            </Button>
+        </main>
+      )}
+    </Authenticator>
+    </div>
+  );
+};
+
+const containerStyle = {
+  width: 400,
+  margin: '20px auto'
+};
+
